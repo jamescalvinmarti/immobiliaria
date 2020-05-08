367bad25a8e8e2caf0906af8f16b162739671756
jim <jimcalvinmarti@gmail.com>
2020-05-08 14:21:53 +0200

    property migration and model

--- app/Zone.php
+++ app/Zone.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Zone extends Model
 {
-    //
+    public function property()
+    {
+        return $this->hasMany('App\Property');
+    }
 }
---
+++ database/migrations/2020_05_08_120203_create_properties_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->double('price');
+            $table->string('description');
+            $table->double('surface');
+            $table->boolean('status');
+            $table->boolean('published');
+            $table->bigInteger('category_id');
+            $table->bigInteger('zone_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
